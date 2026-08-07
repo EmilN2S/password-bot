e734d036c7b2143997a493d69cca53711909fff6
EmilN2S <emil.nazaryan@proton.me>
2026-08-07 19:36:33 +0400

    password generator (work fine btw)

--- src/main.py
+++ src/main.py
@@ -1,7 +1,27 @@
 from os import getenv
 from dotenv import load_dotenv
+import asyncio
+from passwd import passwd
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 
 load_dotenv()
 
 TOKEN = getenv("BOT_TOKEN")
 
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start_handler(message: Message):
+    await message.answer(f"Hi, your password: {passwd(15)}")
+
+
+async def main():
+    bot = Bot(token=TOKEN)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
