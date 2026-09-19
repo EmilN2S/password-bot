e877e544e8c5f015baf2c6fd6e1d4a11ba131678
EmilN2S <emil.nazaryan@proton.me>
2026-09-19 14:16:12 +0400

    added main replykeyboard, 'help' command is work with that

--- src/handlers/help.py
+++ src/handlers/help.py
@@ -1,9 +1,14 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.filters import Command
 from aiogram.types import Message
+from keyboards.main import main_keyboard
 
 router = Router()
 
 @router.message(Command("help"))
+@router.message(F.text.lower() == "help")
 async def help_handler(message: Message):
-    await message.answer(f"Write /start to begin\nWrite /pass *number* for password, as example: /pass 10\nWrite /help for help", parse_mode="Markdown")
+    await message.answer(f"""Write /start to begin\n
+                         Write /pass *number* for password, as example: /pass 10\n
+                         Write /help for help""", 
+                         parse_mode="Markdown", reply_markup=main_keyboard())
---
+++ src/keyboards/main.py
@@ -0,0 +1,11 @@
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+
+def main_keyboard() -> ReplyKeyboardMarkup:
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text="Generate Password")],
+            [KeyboardButton(text="Help")],
+        ],
+        resize_keyboard=True,
+    )
+    return keyboard
