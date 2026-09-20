456d22ed18e1be12c1402be3fc00d1e3f4dc11a9
EmilN2S <emil.nazaryan@proton.me>
2026-09-20 15:23:22 +0400

    changed help output

--- src/handlers/help.py
+++ src/handlers/help.py
@@ -9,7 +9,7 @@ router = Router()
 @router.message(F.text.lower() == "help")
 async def help_handler(message: Message):
     await message.answer("Write /start to begin again\n"
-                         "Write /pass *number* for password\n"
+                         "Write /pass for password\n"
                          "Write /settings for settings\n"
                          "Write /help for help", 
                          parse_mode="Markdown", reply_markup=main_keyboard())
